adde9d3e87611fd20c0c838ec509db120a052365
Ruben63270 <rbf63270@educastur.es>
2026-05-06 20:21:42 +0200

    Add files via upload

--- config/conexion.php
+++ config/conexion.php
@@ -1,1 +1,12 @@
+<?php
+    $servidor = "bbdd"; //En Xampp localhost
+    $usuario = "root";
+    $contraseña = "bbdd"; //En xamppp ""
+    $nombre_bbdd = "Biblioteca";
 
+    $conexion = new mysqli($servidor, $usuario, $contraseña, $nombre_bbdd);
+
+    if($conexion->connect_error){
+        echo "Error en la conexion: " . $conexion->connect_error;
+    }
+?>
